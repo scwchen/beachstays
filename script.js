a7876e9d57acb74eae93f8bd79b351a6f1ec204c
Steven Chen <steven.chen.121@gmail.com>
2021-09-13 18:07:57 -0400

    Adding some pseudo code and comments for ideas on the JS feature.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Adding an image carousel in the gallery on the Beachstays home page
+
+// Add navigation arrows
+    // perhaps with ::before and ::after pseudo elements
+// grab other assets from the page assets to expand the number of photos in the carousel
+// Since the shown images will have to change perhaps add the urls to an array in JS
+    // Or would it be easier to load them all at once and just display them off screen for load times?
+// Add an event listener to the navigation arrows to allow for the updating of the shown images 
