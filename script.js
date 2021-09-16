11037e141b8dd8e91cb019bc48842c8000afc9b8
Steven Chen <steven.chen.121@gmail.com>
2021-09-15 22:49:07 -0400

    Working carousel. Need to replace placeholder images.

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+// ===========================================
 // Adding an image carousel in the gallery on the Beachstays home page
+// ===========================================
 
-// Add navigation arrows
-// perhaps with ::before and ::after pseudo elements
-// grab other assets from the page assets to expand the number of photos in the carousel
-// Since the shown images will have to change perhaps add the urls to an array in JS
-// Or would it be easier to load them all at once and just display them off screen for load times?
-// Add an event listener to the navigation arrows to allow for the updating of the shown images 
-
-// Some test JS for the time being
-let picsArray = [
-    // added a couple of extra pics from the assets folder
-    "./assets/beach-final-images/left-image.jpg",
-    "./assets/beach-final-images/gallery-image1.jpg",
-    "./assets/beach-final-images/gallery-image2.jpg",
-    "./assets/beach-final-images/gallery-image3.jpg",
-    "./assets/beach-final-images/right-image.jpg"];
-
+// Selecting the navigation arrows
 const arrows = document.querySelectorAll('.arrow');
 
-arrows[0].addEventListener('click', function () {
-    getCenterImage().setAttribute('src', picsArray[3]);
+// Add an event listener to the navigation arrows to allow for the updating of the shown images 
+// I wanted to add the same listener to each arrow but slightly tweaked so I used a forEach to apply the same logic to both
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click', function () {
+        
+        // A disable timeout so the carousel buttons are not spammed
+        this.disabled = true;
+        setTimeout(() => {
+            this.disabled = false;
+        }, 500);
+
+        // getting the direction based on whichever button is pretty
+        const direction = this.classList[0].replace('Arrow', '');
+        movePics(direction);
+    });
 });
 
-arrows[1].addEventListener('click', function () {
+// Function to find which of the current image containers is active and visible in the carousel
+const getActive = () => {
+    // forEach apparently does not work for a Nodelist
+    for (image of document.getElementsByClassName('galleryImage')) {
+        if (image.classList[2] === 'active') {
+            return image.classList[1].replace('image', '');
+        }
+    }
+};
 
+const movePics = (direction) => {
+    // getting active index
+    const active = parseInt(getActive());
 
-    getCenterImage().setAttribute('src', picsArray[0]);
-});
+    // getting the selector for the active image
+    const activeImage = document.querySelector(`.image${active}`);
+    
+    if (direction === 'left') {
+        // Only if the carousel is not all the way to the left already
+        if (active !== 1) {
+            // getting the selector for the previous (left) image
+            const prevImage = document.querySelector(`.image${active - 1}`);
+            activeImage.classList.toggle('active');
+            activeImage.classList.toggle('right');
+            // with this logic the prevImage will always be 'left' of the active
+            prevImage.classList.toggle('active');
+            prevImage.classList.toggle('left');
+            
+        }
+    } else if (direction = 'right') {
+        // Only if the carousel is not all the way to the right already
+        if (active !== 5) {
+            // getting the selector for the next (right) image
+            const nextImage = document.querySelector(`.image${active + 1}`);
+            activeImage.classList.toggle('active');
+            activeImage.classList.toggle('left');
+            // with this logic the prevImage will always be 'left' of the active
+            nextImage.classList.toggle('active');
+            nextImage.classList.toggle('right');
+        }
+    }
+    
+    
 
-const getCenterImage = () => {
-    return document.querySelector('.imageCenter img');
-}
-
-// function to shift the pics array and pass an index of the array to the src and alt for the appropriate image
-    // it should also move the tracker left or right accordingly 
-
-// const movePics = (direction) => {
-//     if (direction === 'left') {
-//         picsArray.unshift(picsArray.pop());
-//     } else if (direction === 'right') {
-//         picsArray.push(picsArray.shift());
-//     }
-//     console.log(picsArray);
-// }
+};
